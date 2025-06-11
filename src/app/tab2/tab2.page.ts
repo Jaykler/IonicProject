679892ec50d3cb1f7f03c852bd6778333162da31
Jaykler <julyme10@hotmail.com>
2025-06-10 21:48:29 -0400

    sumadora just fixed

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent,IonItem,IonLabel, IonInput } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { FormsModule } from '@angular/forms';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/compiler';
@@ -8,7 +8,7 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/compiler';
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, FormsModule],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, FormsModule,IonItem,IonLabel, IonInput ],
  
 })
 export class Tab2Page {
@@ -18,10 +18,22 @@ export class Tab2Page {
   operandoB:number = 0;
   resultado:number = 0;
 
+  onNumero1Change(event: any): void {
+    this.operandoA = event.target.value;
+  }
+
+  onNumero2Change(event: any): void {
+    this.operandoB = event.target.value;
+  }
+
   onSumar():void {
     this.resultado = this.operandoA + this.operandoB;
   }
 
-  
+  consoleLog(): void {
+    console.log("Operando A: " + this.operandoA);
+    console.log("Operando B: " + this.operandoB);
+    console.log("Resultado: " + this.resultado);
+  }
 
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent,IonItem,IonLabel, IonInput,IonCardContent,IonCardTitle,IonCard,IonButton,IonCardHeader } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonItem, IonLabel, IonInput, IonCardContent, IonCardTitle, IonCard, IonButton, IonCardHeader,FormsModule],
 })
 export class Tab3Page {
-  constructor() {}
+  numero1: number = 0;
+  numero2: number = 0;
+  resultado: number | null = null;
+
+  sumar() {
+    console.log("Operando A: " + this.numero1);
+    console.log("Operando B: " + this.numero2);
+    return this.resultado = this.numero1 + this.numero2;
+  }
 }
